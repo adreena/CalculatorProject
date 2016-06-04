5fd5e57f6c6220a89623f0029c1327d92dcadb24
Kimia Hassanzadeh <kim.hszd@gmail.com>
2016-06-04 13:53:42 -0600

    added post handler to server

--- AndroidCalculator/app/src/main/java/com/example/khassanz/androidcalculator/MainActivity.java
+++ AndroidCalculator/app/src/main/java/com/example/khassanz/androidcalculator/MainActivity.java
@@ -6,12 +6,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.Buffer;
@@ -21,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     Button btnConnect;
+    Button btnEqualte;
     TextView txtStatus;
+    TextView txtResult;
+    EditText txtEquation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +41,25 @@ public class MainActivity extends AppCompatActivity {
 
         //wiring view to local variables
         btnConnect = (Button)findViewById(R.id.buttonConnect);
-        txtStatus =(TextView)findViewById(R.id.txtViewstatus);
+        btnEqualte = (Button)findViewById(R.id.buttonEqulate);
+        txtStatus =(TextView)findViewById(R.id.txtViewStatus);
+        txtResult = (TextView)findViewById(R.id.txtViewResult);
+        txtEquation = (EditText)findViewById(R.id.editTextEquation);
 
-        btnConnect.setOnClickListener(new View.OnClickListener() {
+        btnEqualte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String status = txtStatus.getText().toString();
+                //String status = txtStatus.getText().toString();
 
-                txtStatus.setText("connected!");
+                //make Post request
+                new PostDataTask().execute("http://192.168.1.70:3000/equlator");
             }
         });
 
         //make Get request
-        new GetDataTask().execute("http://192.168.1.70:3000");
+        new GetDataTask().execute("http://192.168.1.70:3000/");
+
+
     }
 
     class GetDataTask extends AsyncTask<String, Void, String>{
@@ -60,35 +77,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-
-            StringBuilder result = new StringBuilder();
-
             try{
-                //initialize and config request/ then connect to server
-                URL url = new URL(params[0]);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setReadTimeout(10000 /* miliseconds */);
-                urlConnection.setConnectTimeout(10000 /*miliseconds*/);
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setRequestProperty("Content-Type", "text/plain"); //set Header
-                urlConnection.connect();
-
-                //Read data response from server
-                InputStream inputStream = urlConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while((line = bufferedReader.readLine())!=null){
-                    result.append(line).append("\n");
-                }
-
-
-            }catch(IOException ex){
-                return "Netwrok Error Occured!";
-
+                return GetData(params[0]);
             }
-
-
-            return result.toString();
+            catch(IOException ex){
+                return "Netwrok Error Occured!";
+            }
         }
 
         @Override
@@ -103,6 +97,112 @@ public class MainActivity extends AppCompatActivity {
                 progressDialog.dismiss();
             }
         }
+
+        private String GetData(String urlPath) throws IOException{
+            StringBuilder result = new StringBuilder();
+            BufferedReader bufferedReader = null;
+            try {
+                //initialize and config request/ then connect to server
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000 /* miliseconds */);
+                urlConnection.setConnectTimeout(10000 /*miliseconds*/);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Content-Type", "Application/json"); //set Header
+                urlConnection.connect();
+
+                //Read data response from server
+                InputStream inputStream = urlConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = bufferedReader.readLine();
+                /*while((line = bufferedReader.readLine())!=null){
+                    result.append(line).append("\n");
+                }*/
+                result.append(line);
+                if(bufferedReader != null)
+                    bufferedReader.close();
+            }
+            finally{
+                if(bufferedReader!=null)
+                    bufferedReader.close();
+            }
+
+            return result.toString();
+        }
     }
 
+    class PostDataTask extends AsyncTask<String, Void, String>{
+
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Calculating equation ...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try{
+                return postData(params[0]);
+            }catch(IOException ex){
+                return "Network Error";
+            }catch(JSONException ex){
+                return "Data invalid";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            txtResult.setText(result);
+            if(progressDialog != null)
+                progressDialog.dismiss();
+        }
+
+        private String postData(String urlPath) throws IOException,JSONException{
+
+            StringBuilder result = new StringBuilder();
+            BufferedWriter bufferedWriter = null;
+            BufferedReader bufferedReader = null;
+            try {
+                //send equation
+                JSONObject dataToSend = new JSONObject();
+                dataToSend.put("equation", txtEquation.getText().toString());
+
+                //Initialze and config request, connect to server
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000 /* miliseconds */);
+                urlConnection.setConnectTimeout(10000 /*miliseconds*/);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true); /*enable output {body data} */
+                urlConnection.setRequestProperty("Content-Type", "Application/json"); //set Header
+                urlConnection.connect();
+
+                OutputStream outputStream = urlConnection.getOutputStream();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                bufferedWriter.write(dataToSend.toString());
+                bufferedWriter.flush();
+
+                //read data response from server
+                InputStream inputStream = urlConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    result.append(line).append("\n");
+                }
+            }finally{
+                if(bufferedWriter !=null)
+                    bufferedWriter.close();
+                if(bufferedReader != null)
+                    bufferedReader.close();
+            }
+            return result.toString();
+        }
+
+    }
 }
--- CalculatorServer/server.js
+++ CalculatorServer/server.js
@@ -1,18 +1,67 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var port = 3000;
+var engines = require('consolidate');
+var bodyParser = require('body-parser');
+
+
 //express middleware
 var app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+//app.use(;
 
+function errorHandler(err,req,res,next){
+	console.error(err.message);
+	console.error(err.stack);
+	res.status(500);
+	res.rencer(error_template);
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//setting up template engine
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname+ '/views');
+
+
+var router = express.Router();
+
+router.route('/equlator')
+	  .get(function(req,res){
+	  	res.render('index'); 
+
+	  })
+	 .post(function(req,res,next){
+	 	//req.checkBody('equation','equation field is required').notEmpty();
+	  	var equation = req.body.equation;
+	  	if(typeof equation == 'undefined'){
+	  		next(Error('Please type yout equation'));
+	  	}else{
+			/*res.render('index',{
+								'result': equation,
+								'length': equation.length}); */
+								res.send({
+								'result': equation,
+								'length': equation.length});
+			
+	  	}
+	  });
 
 //routes:
-app.use('/',function(request, response){
-	response.send('Connected to Calculator Servers');
+app.use(errorHandler);
+app.use('/',router);
+app.get('/',function(req, res){
+	res.send('Connected to Equlator Server !');
 });
 
-app.listen(port);
-console.log('server connected....');
+app.use(function(req,res){
+	res.sendStatus(404);
+})
+
+
+app.listen(port, function(){
+	console.log('server is running on port:'+port);
+});
+
 
 
